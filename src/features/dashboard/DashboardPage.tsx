0e836aa824dce649137c11a258bc0e286871f806
froma <github@froma.dev>
2025-04-21 20:42:06 +0200

    add dashboard page and overview card

---
+++ src/features/dashboard/DashboardPage.tsx
@@ -0,0 +1,46 @@
+import OverviewCard from "./OverviewCard";
+import { shortNumber } from "@/utils/utils";
+
+interface OverviewCardData {
+  title: string;
+  description: string;
+  trend: number;
+}
+const dashboardTitle = "Channel Overview";
+const overviewCardsData: OverviewCardData[] = [
+  {
+    title: shortNumber(15000),
+    description: "Subscribers",
+    trend: 150,
+  },
+  {
+    title: shortNumber(10000),
+    description: "Total views",
+    trend: -5.5,
+  },
+  {
+    title: shortNumber(100000),
+    description: "Total likes",
+    trend: 100,
+  },
+];
+const DashboardPage = () => {
+  return (
+    <section className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold text-white">{dashboardTitle}</h1>
+      <div className="flex gap-4">
+        {overviewCardsData.map((card) => (
+          <OverviewCard
+            key={card.description}
+            title={card.title}
+            description={card.description}
+            trend={card.trend}
+            className={`basis-1/${overviewCardsData.length}`}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default DashboardPage;
